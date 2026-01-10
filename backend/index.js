d0b138203639ff2c3383ced4510f00d05265452a
Deeepesh giri <dr.deepeshgiri@gmail.com>
2026-01-11 01:15:44 +0530

    testing CI/CD deploy

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// API route
 app.get('/', (req, res) => {
-  res.json({ message: 'Backend API running' });
+  res.json({ message: 'Backend API running', time: new Date() });
 });
 
+// Listen on all interfaces
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
